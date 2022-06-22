78994342bb31796cb228ba57187e350c937f1be6
jzx <945627077@qq.com>
2022-06-22 12:14:17 +0800

    更新

--- src/i18n/lang/en.ts
+++ src/i18n/lang/en.ts
@@ -1,6 +1,8 @@
 const en = {
   title: "SpringMall",
   slogan: "Another open source e-commerce program",
+  account: "Account",
+  password: "Password",
 };
 
 export default en;
--- src/i18n/lang/zh.ts
+++ src/i18n/lang/zh.ts
@@ -1,6 +1,9 @@
 const zh = {
   title: "SpringMall",
   slogan: "Another open source e-commerce program",
+  account: "账户:",
+  password: "密码:",
+  login_btn: "登录",
 };
 
 export default zh;
